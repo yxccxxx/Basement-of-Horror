2173912b508361821a19dfee87d5ac3db01b05d9
Yixin Chen <yixincindy19@gmail.com>
2018-07-30 12:29:37 -0700

    timeline and animation logics working

--- Assets/Script/Animation1Start.cs
+++ Assets/Script/Animation1Start.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
 
 public class Animation1Start : MonoBehaviour {
-    public GameObject door;
+    public GameObject go;
+    private bool initial = true;
     // Update is called once per frame
     void Update () {
         bool touched = OculusTrigger.touched;
-        if (touched)
+        if (touched && initial)
         {
-            //Debug.Log("touched!!!!!!");
-            // animation begins from here
-            //Animator anim = door.GetComponent<Animator>();
-            //anim.Play("front-door-pivot");
+            initial = false;
+            go.GetComponent<PlayableDirector>().Play();
         }
 	}
 }
