c70a74d8d9b12a1f3b6ded59adc9277f2abf35f8
Yixin Chen <yixincindy19@gmail.com>
2018-07-30 17:49:00 -0700

    animation 1

---
+++ Assets/Script/KeyboardTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardTrigger : MonoBehaviour {
+    public static bool touched = false;
+    public GameObject leftController;
+    public GameObject rightController;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
