e6a73e98016f9dc0d4a601c4c280c005618caa50
Yixin Chen <yixincindy19@gmail.com>
2018-07-30 20:01:23 -0700

    with timeline 2 working

---
+++ Assets/Script/Animation1Start.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class Animation1Start : MonoBehaviour {
+    public GameObject go;
+    private bool initial = true;
+    // Update is called once per frame
+    void Update () {
+        bool touched = OculusTrigger.touched;
+        if (touched && initial)
+        {
+            initial = false;
+            go.GetComponent<PlayableDirector>().Play();
+        }
+	}
+}
---
+++ Assets/Script/Animation2Start.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class Animation2Start : MonoBehaviour {
+    public GameObject go;
+    private bool initial = true;
+	
+	// Update is called once per frame
+	void Update () {
+        bool touched = customTouch.touchKeyBD;
+        if(touched && initial)
+        {
+            initial = false;
+            go.GetComponent<PlayableDirector>().Play();
+        }
+	}
+}
